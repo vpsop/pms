92393d83e6146f759ed7d9d969de81d797cb0d35
vpsop <vpsop544@gmail.com>
2024-02-01 22:35:37 +0530

    updated the addOpenings to also add company data to database

--- firebase/add-opening.ts
+++ firebase/add-opening.ts
@@ -1,4 +1,5 @@
 import { doc, Firestore, getDoc, setDoc } from "firebase/firestore";
+import getCompanyDataFromSlug from "./get-company-data";
 
 export default async function addOpening(
     db:Firestore, 
@@ -15,19 +16,26 @@ export default async function addOpening(
 
         // a ref to the expected doc
         var docRef = doc(db, "openings", slug);
+        var companyData = await getCompanyDataFromSlug(db, company);
 
         // check if a doc with expected slug already exist
         await getDoc(docRef).then(async (doc) => {
             if(doc.exists()) {
                 // Document already exist;
                 error = "A opening with this slug already exist";
+            } else if (companyData.result === undefined) {
+                error = "Failed to get data of selected company data from server.";
+            } else if (companyData.error != null) {
+                error = "Company data error is not null" + companyData.error;
             } else {
                 // Document does not exist, create this
                 await setDoc(docRef, {
                     position: positionName,
                     jobDescription: description,
-                    company,
+                    companySlug: company,
                     location,
+                    companyName: companyData.result["name"],
+                    companyImageURL: companyData.result["imageURL"]
                 });
                 result = "Opening added successfully"
             }
