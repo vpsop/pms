dbba836175ba579b0b605d6312f4a4d5b2192561
vpsop <vpsop544@gmail.com>
2024-02-01 22:35:09 +0530

    fixes to function

--- firebase/get-company-data.ts
+++ firebase/get-company-data.ts
@@ -1,4 +1,5 @@
 import { doc, DocumentData, Firestore, getDoc } from "firebase/firestore";
+import { COMPANIES_COLLECTION } from "./config";
 
 export default async function getCompanyDataFromSlug(
     db:Firestore, 
@@ -9,22 +10,26 @@ export default async function getCompanyDataFromSlug(
 
     try {
         // a ref to the expected doc
-        var docRef = doc(db, "openings", slug);
-
+        var docRef = doc(db, COMPANIES_COLLECTION, slug);
+        console.log("SEARCHING COMPANY DATA FOR: ", slug);
+        
         // check if a doc with expected slug already exist
         await getDoc(docRef).then(async (doc) => {
-            if(doc.exists()) {
+            if (!doc.exists()) {
                 // Document already exist;
                 error = "Company with this ID does not exist.";
             } else {
                 // Document does not exist, create this
-                
-                result = doc.data();
+                if (doc.data() !== undefined) {
+                    result = doc.data()!;
+                }
             }
         });
     } catch (err) {
         error = err;
     }
 
+    if (error) { console.log("GET COMPANY DATA ERROR: ", error); }
+    
     return { result, error };
 }
